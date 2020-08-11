a5c417d78456f9fc9f16cdf019ac0606d85e7343
Phuc Nguyen <kobaogiopo2@gmail.com>
2020-08-11 19:34:51 +0700

    reverse

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,83 +1,72 @@
+const user = require("../models/home.model");
+const { render } = require("pug");
+const { response } = require("express");
+const moment = require("moment-timezone");
 
-const user = require('../models/home.model');
-const { render } = require('pug');
-const { response } = require('express');
-const moment = require('moment-timezone');
+module.exports.home = async (req, res) => {
+  let a = req.session.idUsername;
+  let users = await user.find({ _id: a });
+  users[0].plans.reverse();
+  res.render("home", { plans: users });
+};
 
-module.exports.home = async (req,res) =>{
-    let a = req.session.idUsername;
-    let users = await user.find({_id: a})
-    res.render('home',{plans:users});
-}
+module.exports.out = (req, res) => {
+  let out = req.body.Out;
+  if (out == "1") {
+    req.session.destroy((err) => {
+      console.log("K Xong");
+    });
+    res.redirect("/");
+  }
+  // }else{
+  //     console.log("K Xog");
+  //     res.redirect('/');
+  // }
+};
 
-module.exports.out = (req,res) =>{
-    let out = req.body.Out;
-    if(out == '1'){
-        req.session.destroy( (err) =>{
-            console.log("K Xong");
-        });
-        res.redirect('/');
+module.exports.confirmDelete = (req, res) => {
+  let idMain = req.body.idMain;
+  let id = req.body.id;
+  let deleteId = { _id: id };
+  user.findByIdAndUpdate(
+    { _id: idMain },
+    { $pull: { plans: deleteId } },
+    (err, success) => {
+      if (err) {
+        console.log("Err");
+      } else {
+        console.log("Success");
+      }
     }
-    // }else{
-    //     console.log("K Xog");
-    //     res.redirect('/');
-    // }
+  );
+  res.redirect("/");
+};
 
-}
+module.exports.confirmComplete = (req, res) => {
+  let idMain = req.body.idMain;
+  let id = req.body.id;
+  console.log(id);
+  console.log(idMain);
+  let count = parseInt(req.body.countConfirm) + 1;
+  let newPlan = { "plans.$.count": count };
+  user.update(
+    { _id: idMain, "plans._id": id },
+    { $set: newPlan },
+    (err, success) => {
+      if (err) {
+        console.log("Err");
+      } else {
+        console.log("Success");
+      }
+    }
+  );
+  res.redirect("/");
+};
 
-module.exports.confirmDelete = (req,res) =>{
-    let idMain = req.body.idMain;
-    let id = req.body.id
-    let deleteId = {'_id':id};
-    user.findByIdAndUpdate(
-        {_id:idMain},
-        {$pull: {'plans':deleteId}},
-        (err, success) =>{
-            if(err){
-                console.log("Err");
-            }else{
-                console.log("Success");
-            }
-        }
-    )
-    res.redirect('/');
-}
-
-
-
-module.exports.confirmComplete = (req,res)=>{
-    let idMain = req.body.idMain;
-    let id = req.body.id;
-    console.log(id);
-    console.log(idMain);
-    let count = parseInt(req.body.countConfirm) + 1;
-    let newPlan = {'plans.$.count':count};
-    user.update(
-        {_id:idMain,'plans._id':id},
-        {$set:newPlan},
-        (err, success) =>{
-            if(err){
-                console.log("Err");
-            }else{
-                console.log("Success");
-            }
-        }
-    )
-    res.redirect('/');
-}
-
-
-
-module.exports.homePost = async (req,res) =>{
-    let planNewName = req.body.nameCreate;
-    let idMain = req.body.idCreate;
-    let newPlan = {"namePlan":planNewName,"count":0,"date": new Date()};
-    user.findByIdAndUpdate(
-        {_id:idMain},
-        {$push:{plans:newPlan}},
-    ).exec();
-    res.redirect('/');
-}
-
-
-
+module.exports.homePost = async (req, res) => {
+  let planNewName = req.body.nameCreate;
+  let idMain = req.body.idCreate;
+  let newPlan = { namePlan: planNewName, count: 0, date: new Date() };
+  user.findByIdAndUpdate({ _id: idMain }, { $push: { plans: newPlan } }).exec();
+  res.redirect("/");
+};
